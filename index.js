911df7533bcd0bbbd3edf321c130c1cb0a3b682d
Yoshi Maisami <yoshi.maisami@gmail.com>
2019-02-27 11:12:36 -0500

    fixed problem with diplaying Konnichiwa Sekai on port 3300

--- index.js
+++ index.js
@@ -6,3 +6,12 @@ app.get("/", function(req, res) {
 });
 
 app.listen(3300, () => console.log("listening on port 3300"));
+
+const express = require("express");
+const app = express();
+
+app.get("/", function(req, res) {
+  res.send("Kon'nichiwa Sekai!");
+});
+
+app.listen(3300, () => console.log("listening on port 3300"));
