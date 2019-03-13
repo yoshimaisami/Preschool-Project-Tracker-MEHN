aca5d82c0c10216578dcfb6514d873abd309ce42
Yoshi Maisami <yoshi.maisami@gmail.com>
2019-03-13 17:52:23 -0400

    heroku redo

--- db/connection.js
+++ db/connection.js
@@ -1,24 +1,31 @@
 const mongoose = require("mongoose");
 
 if (process.env.NODE_ENV == "production") {
-  mongoose.connect(process.env.DB_URL, { useNewUrlParser: true }, function(
-    err
-  ) {
-    if (err) {
-      return console.log(err);
-    }
-  });
+  mongoose.connect(process.env.DB_URL);
 } else {
-  mongoose.connect(
-    "mongodb://localhost/lessonplans",
-    { useNewUrlParser: true },
-    function(err) {
-      if (err) {
-        return console.log(err);
-      }
-    }
-  );
+  mongoose.connect("mongodb://localhost/lessonplans");
 }
+
+// if (process.env.NODE_ENV == "production") {
+//   mongoose.connect(process.env.DB_URL, { useNewUrlParser: true }, function(
+//     err
+//   ) {
+//     // if (err) {
+//     //   return console.log(err);
+//     // }
+//   });
+// } else {
+//   mongoose.connect(
+//     "mongodb://localhost/lessonplans",
+//     { useNewUrlParser: true },
+//     function(err) {
+//       if (err) {
+//         return console.log(err);
+//       }
+//     }
+//   );
+// }
+
 mongoose.Promise = Promise;
 
 module.exports = mongoose;
