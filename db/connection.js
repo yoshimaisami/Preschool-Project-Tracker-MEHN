72daf80912370250792f73a3faa4f323efeccaa0
Yoshi Maisami <yoshi.maisami@gmail.com>
2019-03-13 19:49:33 -0400

    trying useNewUrlParser: true... on remote and local

--- db/connection.js
+++ db/connection.js
@@ -1,7 +1,10 @@
 const mongoose = require("mongoose");
 
 if (process.env.NODE_ENV == "production") {
-  mongoose.connect(process.env.DB_URL);
+  mongoose.connect(process.env.DB_URL),
+    {
+      useNewUrlParser: true
+    };
 } else {
   mongoose.connect("mongodb://localhost/lessonplans", {
     useNewUrlParser: true
