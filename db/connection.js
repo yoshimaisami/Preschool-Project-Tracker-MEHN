34b9798b2cb236c2d83540c08677b6d466dbd3ee
Yoshi Maisami <yoshi.maisami@gmail.com>
2019-03-13 17:57:50 -0400

    more heroku

--- db/connection.js
+++ db/connection.js
@@ -3,7 +3,9 @@ const mongoose = require("mongoose");
 if (process.env.NODE_ENV == "production") {
   mongoose.connect(process.env.DB_URL);
 } else {
-  mongoose.connect("mongodb://localhost/lessonplans");
+  mongoose.connect("mongodb://localhost/lessonplans", {
+    useNewUrlParser: true
+  });
 }
 
 // if (process.env.NODE_ENV == "production") {
